00ea26d5eec644772842151cdef67280dc786015
Nicoleta <nicoletavoinea17@yahoo.com>
2022-05-06 19:26:30 +0300

    AddMoney interface was added

--- VirtualCurrencyWallet_SEF/src/main/java/com/example/virtualcurrencywallet_sef/AddMoney.java
+++ VirtualCurrencyWallet_SEF/src/main/java/com/example/virtualcurrencywallet_sef/AddMoney.java
@@ -1,4 +1,30 @@
 package com.example.virtualcurrencywallet_sef;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+
 public class AddMoney {
+    @FXML
+    private Label label_invalidcredentials;
+    @FXML
+    private Label label_successful;
+    @FXML
+    private TextField field_sum;
+    @FXML
+    private TextField field_cardnumber;
+    @FXML
+    private TextField field_pin;
+    @FXML
+    private Button button_back;
+
+    @FXML
+    private void back(ActionEvent event) throws IOException {
+        Main m=new Main();
+        m.changeScene("Menu.fxml");
+    }
 }
