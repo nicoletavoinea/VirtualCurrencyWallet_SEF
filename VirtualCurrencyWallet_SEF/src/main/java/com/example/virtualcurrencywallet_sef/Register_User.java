1e0874e57559bfb6d5271622b253cf6aea775440
Vlad <big_vlad2002@yahoo.com>
2022-05-06 10:12:06 +0300

    Added registration page to GUI

---
+++ VirtualCurrencyWallet_SEF/src/main/java/com/example/virtualcurrencywallet_sef/Register_User.java
@@ -0,0 +1,47 @@
+package com.example.virtualcurrencywallet_sef;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class Register {
+    @FXML
+    Label label_InvalidAge;
+    @FXML
+    TextField field_FullName;
+    @FXML
+    TextField field_ID;
+    @FXML
+    TextField field_PhoneNumber;
+    @FXML
+    TextField field_HomeAdress;
+    @FXML
+    TextField field_CardNumber;
+    @FXML
+    PasswordField field_PIN;
+    @FXML
+    TextField field_Username;
+    @FXML
+    PasswordField field_Password;
+    @FXML
+    Button button_Register;
+    @FXML
+    Button button_Back;
+
+    public Register(){}
+
+    @FXML
+    public void register(ActionEvent event) throws Exception{
+        Main m= new Main();
+        m.changeScene("Menu.fxml");
+    }
+
+    @FXML
+    public void back(ActionEvent event) throws Exception{
+        Main m= new Main();
+        m.changeScene("LogIn.fxml");
+    }
+}
