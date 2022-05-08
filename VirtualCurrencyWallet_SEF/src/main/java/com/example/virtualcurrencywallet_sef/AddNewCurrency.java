e2864834d150bc8faac3cc375f41127eaa9654a6
Vlad <big_vlad2002@yahoo.com>
2022-05-08 19:26:23 +0300

    Added  "Add new currency" page in the admin menu

--- VirtualCurrencyWallet_SEF/src/main/java/com/example/virtualcurrencywallet_sef/AddNewCurrency.java
+++ VirtualCurrencyWallet_SEF/src/main/java/com/example/virtualcurrencywallet_sef/AddNewCurrency.java
@@ -1,4 +1,36 @@
 package com.example.virtualcurrencywallet_sef;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+
+
 public class AddNewCurrency {
+    @FXML
+    private TextField field_CurrencyName;
+    @FXML
+    private TextField field_CurrencyRate;
+    @FXML
+    private Label label_AlreadyExists;
+    @FXML
+    private Button button_AddCurrency;
+    @FXML
+    private Button button_Back;
+
+    public AddNewCurrency(){}
+
+    @FXML
+    public void addCurrency(ActionEvent event) throws IOException{
+        label_AlreadyExists.setText("Currency added");
+    }
+
+    @FXML
+    public void back(ActionEvent event) throws IOException{
+        Main m=new Main();
+        m.changeScene("Menu_Admin.fxml");
+    }
 }
