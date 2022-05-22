70340dbd303e66b013490eaa4bdba29c5a00198a
nicoletavoinea <nicoletavoinea17@yahoo.com>
2022-05-22 22:51:06 +0300

    Added exceptions for text fields in User Registration page and in Admin Registration Page

--- VirtualCurrencyWallet_SEF/src/main/java/com/example/virtualcurrencywallet_sef/Authentication/Register_User.java
+++ VirtualCurrencyWallet_SEF/src/main/java/com/example/virtualcurrencywallet_sef/Authentication/Register_User.java
@@ -14,6 +14,7 @@ import org.json.simple.JSONArray;
 import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
+import java.math.BigInteger;
 
 public class Register_User {
     @FXML
@@ -43,7 +44,7 @@ public class Register_User {
 
     @FXML
     public void register(ActionEvent event) throws Exception{
-        if(checkEmptyFields()==0 && checkValidInfo()){
+        if(checkInvalidFields()==0){
             FileHandler fileHandler=new FileHandler("src/main/java/com/example/virtualcurrencywallet_sef/Database/Users.json");
             JSONArray jsonArray=fileHandler.read();
             User user=new User(field_FullName.getText(),field_ID.getText(),field_PhoneNumber.getText(),field_HomeAdress.getText(),field_CardNumber.getText(),field_PIN.getText(),field_Username.getText(),field_Password.getText());
@@ -63,53 +64,97 @@ public class Register_User {
         m.changeScene("LogIn.fxml");
     }
 
-    public int checkEmptyFields(){
+    public int checkInvalidFields() throws IOException, ParseException {
+        int number;
+        BigInteger bignumber;
+        User user=new User();
+
         if(field_FullName.getText().isEmpty()){
             label_Error.setText("You must enter your full name");
             return 1;
         }
-        else if (field_ID.getText().isEmpty()){
-            label_Error.setText("You must enter your ID");
+        if (field_ID.getText().isEmpty()){
+                label_Error.setText("You must enter your ID");
+                return 1;
+        }
+        else try{
+            bignumber=new BigInteger(field_ID.getText());
+            System.out.println(bignumber);
+        }catch (NumberFormatException e){
+            label_Error.setText("ID can not contain letters or special characters");
             return 1;
         }
-        else if (field_PhoneNumber.getText().isEmpty()){
+        if(field_ID.getText().length()!=13){
+            label_Error.setText("ID must be 13 digits long");
+            return 1;
+        }
+        if(user.isUnderage(field_ID.getText())){
+            label_Error.setText("User can not be underage");
+            return 1;
+        }
+        if (field_PhoneNumber.getText().isEmpty()){
             label_Error.setText("You must enter your phone number");
             return 1;
         }
-        else if(field_HomeAdress.getText().isEmpty()){
+        else try{
+            bignumber=new BigInteger(field_PhoneNumber.getText());
+            System.out.println(bignumber);
+        }catch (NumberFormatException e){
+            label_Error.setText("Phone number can not contain letters or special characters");
+            return 1;
+        }
+        if(field_PhoneNumber.getText().length()!=10){
+            label_Error.setText("Phone number must be 10 digits long");
+            return 1;
+        }
+
+        if(field_HomeAdress.getText().isEmpty()){
             label_Error.setText("You must enter your home address");
             return 1;
         }
-        else if(field_CardNumber.getText().isEmpty()){
+        if(field_CardNumber.getText().isEmpty()){
             label_Error.setText("You must enter your card number");
             return 1;
         }
-        else if(field_PIN.getText().isEmpty()){
+        else try{
+            bignumber=new BigInteger(field_CardNumber.getText());
+            System.out.println(bignumber);
+        }catch (NumberFormatException e){
+            label_Error.setText("Card number can not contain letters or special characters");
+            return 1;
+        }
+        if(field_CardNumber.getText().length()!=16){
+            label_Error.setText("Card number must be 16 digits long");
+            return 1;
+        }
+        if(field_PIN.getText().isEmpty()){
             label_Error.setText("You must enter your card PIN");
             return 1;
         }
-        else if(field_Username.getText().isEmpty()){
+        else try{
+            System.out.println(Integer.parseInt(field_PIN.getText()));
+        }catch (NumberFormatException e){
+            label_Error.setText("PIN can not contain letters or special characters");
+            return 1;
+        }
+        if(field_PIN.getText().length()!=4){
+            label_Error.setText("PIN number must be 4 digits long");
+            return 1;
+        }
+        if(field_Username.getText().isEmpty()){
             label_Error.setText("You must choose an username");
             return 1;
         }
-        else if(field_Password.getText().isEmpty()){
-            label_Error.setText("Password can't be empty");
+        if(user.usernameExists(field_Username.getText())!=-1) {
+            label_Error.setText("Username already exists :( Try a new one");
+            return 1;
+        }
+        if(field_Password.getText().isEmpty()){
+            label_Error.setText("Password can not be null");
             return 1;
         }
 
         return 0;
     }
 
-    public boolean checkValidInfo() throws IOException, ParseException {
-        User user=new User();
-        if(user.usernameExists(field_Username.getText())!=-1) {
-            label_Error.setText("Username already exists :( Try a new one");
-            return false;
-        }
-        if(user.isUnderage(field_ID.getText())){
-            label_Error.setText("User can not be underage");
-            return false;
-        }
-        return true;
-    }
 }
