b715ce1eaeba4200490cd693177e6612b708c958
nicoletavoinea <nicoletavoinea17@yahoo.com>
2022-05-24 21:51:04 +0300

    Unit tests for Admin and User classes

---
+++ VirtualCurrencyWallet_SEF/Test/com/example/virtualcurrencywallet_sef/Model/AdminTest.java
@@ -0,0 +1,13 @@
+package com.example.virtualcurrencywallet_sef.Model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdminTest {
+    @Test
+    void adminJSONTest(){
+
+    }
+
+}
